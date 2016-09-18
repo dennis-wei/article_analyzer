4908a3e727f7d901f9ebf3cf3c34ff90001a99ea
Dennis Wei <dennis@dyn-160-39-142-130.dyn.columbia.edu>
2016-09-18 16:00:57 -0400

    deleted unneeded files

--- jobs/services/get_postfix.py
+++
@@ -1,9 +0,0 @@
-def wp_post(url):
-    sections = url.split('/')
-    return max(sections, key=len)
-
-def nyt_post(url):
-    sections = url.split('/')
-    section = filter(lambda u: '.html' in u, sections)[0]
-    idx = section.find('.html')
-    return section[:idx]
